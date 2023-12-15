b5596a4e0b726ef2b9ba95103dcb5e1e988beeb9
Eng-Prud <Prudence.n826@gmai.com>
2023-12-15 14:07:00 -0800

    rectify header file

---
+++ mod_opcode.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+/**
+ * f_mod - Function that divides the second
+ * top element of the stack by the top element of the stack
+ * @head: the stack head
+ * @counter: line_number
+*/
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *home;
+	int length = 0, auxiliary;
+
+	home = *head;
+	while (home)
+	{
+		home = home->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	home = *head;
+	if (home->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	auxiliary = home->next->n % home->n;
+	home->next->n = auxiliary;
+	*head = home->next;
+	free(head);
+}
